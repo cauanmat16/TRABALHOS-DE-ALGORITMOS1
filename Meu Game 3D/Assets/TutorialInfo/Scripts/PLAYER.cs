a06ab54e610e015efb3c9b1052886487b5dbca52
cauanmat16 <cauan.mateus.mateus@gmail.com>
2024-08-27 11:07:41 -0300

    jogo quase feito

--- Meu Game 3D/Assets/TutorialInfo/Scripts/PLAYER.cs
+++ Meu Game 3D/Assets/TutorialInfo/Scripts/PLAYER.cs
@@ -7,25 +7,46 @@ using UnityEngine.SceneManagement;
 public class PLAYER : MonoBehaviour
 {
     public int velocidade = 10;
-    public Rigidbody rb; 
+    public int ForcaPulo = 10;
+    public bool noChao = false;
+
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(" start ");
-        TryGetComponent( out rb);
+        TryGetComponent(out rb);
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Chão")
+        {
+            noChao = true;
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
         Debug.Log("  update ");
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
+
         Vector3 direção = new Vector3(x, y: 0, z: y);
         rb.AddForce(direção * velocidade * Time.deltaTime, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * ForcaPulo, ForceMode.Impulse);
+        }
+
         if (transform.position.y < -5)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
         }
     }
 }
+
+
+
